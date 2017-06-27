064825689547e3a8fc37ae09f1a05ea0514b3718
Payal <payalghoorhoo@gmail.com>
2017-06-27 13:32:59 +0400

    improved version of routing

--- app/routing.js
+++ app/routing.js
@@ -2,42 +2,41 @@
     'use strict';
 
     angular
-        .module('formDesigner')
+        .module('formDesigner', ['ui.router'])
         .config(RoutingConfig);
 
     function RoutingConfig($stateProvider, $urlRouterProvider) {
-
+        
         // use the HTML5 History API
         //$locationProvider
         //    .html5Mode(true);
 
         // Default location to navigate to start with... 
         $urlRouterProvider
-            .when('', '/')
+            .when('', '/home')
             .otherwise('/home');
     
         $stateProvider
-
         .state('home',{
-            url:'/view',
+            url:'/home',
             templateUrl:'app/view/home.html'
         })
 
-        .state('listOfForms',{
-            url:'/view',
-            templateUrl:'app/view/listOfForm.html',
-            controller:'formgrid'
+        .state('formList',{
+            url:'/formList',
+            templateUrl:'app/view/formList.html',
+            controller:'formList'
         })
 
-        .state('editForm',{
-            url:'/view',
-            templateUrl:'app/view/editForm.html',
-            controller:'editctrl'
+        .state('formEdit',{
+            url:'/formEdit',
+            templateUrl:'app/view/formEdit.html',
+            controller:'formEdit'
         })
 
         .state('formPreview',{
-            url:'/view',
-            templateUrl:'app/view/formPreview',
+            url:'/formPreview',
+            templateUrl:'app/view/formPreview.html',
             controller:'formPreview'
         })  
     }
