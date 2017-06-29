d90f28da79ae5b3ed7bad67b501de2aa21909ac2
Payal <payalghoorhoo@gmail.com>
2017-06-29 11:30:31 +0400

    adding tabs to side nav

--- app/controller/formEdit.js
+++ app/controller/formEdit.js
@@ -1,56 +1,26 @@
-
- /** formDesignerApp.controller('formEdit', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
-    $scope.toggleLeft = buildToggler('left');
- 
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     
-    function buildToggler(navID) {
-      var debounceFn =  $mdUtil.debounce(function(){
-            $mdSidenav(navID)
-              .toggle()
-              .then(function () {
-                $log.debug("toggle " + navID + " is done");
-              });
-          },200);
-      return debounceFn;
-    }
-
-    formDesignerApp.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-    };
-  })
-});*/
-
-
-  formDesignerApp.controller('formEdit', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
-    $scope.toggleLeft = buildToggler('left');
-   
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildToggler(navID) {
-      var debounceFn =  $mdUtil.debounce(function(){
-            $mdSidenav(navID)
-              .toggle()
-              .then(function () {
-                $log.debug("toggle " + navID + " is done");
-              });
-          },200);
-      return debounceFn;
-    }
-  })
-  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-    };
-  })
+formDesignerApp.controller('formEdit', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
+  $scope.toggleLeft = buildToggler('left');
+  
+  /**
+   * Build handler to open/close a SideNav; when animation finishes
+   * report completion in console
+   */
+  function buildToggler(navID) {
+    var debounceFn =  $mdUtil.debounce(function(){
+          $mdSidenav(navID)
+            .toggle()
+            .then(function () {
+              $log.debug("toggle " + navID + " is done");
+            });
+        },200);
+    return debounceFn;
+  }
+})
+.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function () {
+    $mdSidenav('left').close()
+      .then(function () {
+        $log.debug("close LEFT is done");
+      });
+  };
+})
