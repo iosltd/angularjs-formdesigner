959bba271afea1fe3ce60045e86ed049cd2fda30
Nirvan <nshewraj16@gmail.com>
2017-06-30 11:11:58 +0400

    update dummy data to add dates

--- app/controller/formList.js
+++ app/controller/formList.js
@@ -5,13 +5,13 @@ function formList ($scope) {
             dataSource: {    
                 data: 
                 [
-                    { formList: "Form 1"},
-                    { formList: "Form 2"},
-                    { formList: "Form 3"},
-                    { formList: "Form 4"},
-                    { formList: "Form 5"},
-                    { formList: "Form 6"},
-                    { formList: "Form 7"},
+                    { formList: "Form 1", dateModified:"10/08/2014" },
+                    { formList: "Form 2", dateModified:"25/01/2009" },
+                    { formList: "Form 3", dateModified:"29/07/2012" },
+                    { formList: "Form 4", dateModified:"08/03/2004" },
+                    { formList: "Form 5", dateModified:"17/11/2016" },
+                    { formList: "Form 6", dateModified:"22/09/2017" },
+                    { formList: "Form 7", dateModified:"24/05/2010" },
                 ],
             },
 
