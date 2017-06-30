f5083e52db7123242446d505248244538fb8638b
Nirvan <nshewraj16@gmail.com>
2017-06-30 14:58:45 +0400

    Add expand row option and restructure form definitons (dummy data)

--- app/controller/formList.js
+++ app/controller/formList.js
@@ -2,28 +2,68 @@ formDesignerApp.controller('formList', formList);
 
 function formList ($scope) {
     $scope.mainGridOptions = {
-            dataSource: {    
-                data: 
-                [
-                    { forms: "Form 1", dateModified:"10/08/2014" },
-                    { forms: "Form 2", dateModified:"25/01/2009" },
-                    { forms: "Form 3", dateModified:"29/07/2012" },
-                    { forms: "Form 4", dateModified:"08/03/2004" },
-                    { forms: "Form 5", dateModified:"17/11/2016" },
-                    { forms: "Form 6", dateModified:"22/09/2017" },
-                    { forms: "Form 7", dateModified:"24/05/2010" },
-                ],
-            },
+            dataSource: [
+                {
+                     forms: "Form 1",
+                        date: [
+                            { dateModified: "10/08/2014" }
+                        ]
+                },
+                {
+                    forms: "Form 2",
+                        date: [
+                            { dateModified: "25/01/2009" }
+                        ]
+                },
+                 {
+                    forms: "Form 3",
+                        date: [
+                            { dateModified: "29/07/2012" }
+                        ]
+                },
+                 {
+                    forms: "Form 4",
+                        date: [
+                            { dateModified: "08/03/2004" }
+                        ]
+                },
+                 {
+                    forms: "Form 5",
+                        date: [
+                            { dateModified: "17/11/2016" }
+                        ]
+                },
+                {
+                     forms: "Form 6",
+                        date: [
+                            { dateModified: "22/09/2017" }
+                        ]
+                },
+                {
+                     forms: "Form 7",
+                        date: [
+                            { dateModified: "24/05/2010" }
+                        ]
+                },
+                
+            ],
 
             sortable: true,
             pageable: true,
             resizable: true,
             selectable: true,
             pageable: {
-                pageSize: 2,
+                pageSize: 4,
                 previousNext: true,
                 input: true,
                 pageSizes: true,
+            },
+
+            detailTemplate: 'Form Details: <div class="grid"></div>',
+            detailInit: function(e) {
+                e.detailRow.find(".grid").kendoGrid({
+                dataSource: e.data.date
+                });
             },
 
             columns: 
